a3ae8a6104e2eb1b7182424326734f48c153bf67
AmrElsaied <55893916+AmrElsaied@users.noreply.github.com>
2021-10-07 22:47:34 +0200

    Add files via upload

---
+++ Embedded/AVR/ATMEGA32/HAL_Drivers/KeyPad_Driver/Keybad_Priv.h
@@ -0,0 +1,19 @@
+/*
+ * Keybad_Priv.h
+ *
+ *  Created on: Feb 8, 2020
+ *      Author: Amr Elsaied
+ */
+
+#ifndef KEYBAD_PRIV_H_
+#define KEYBAD_PRIV_H_
+
+#define Invalid_Port_Min -1
+#define Invalid_Port_Max  4
+#define Success 1
+#define Failed  0
+#define RowMaxPinNumber  3
+#define Pressed 0
+
+
+#endif /* KEYBAD_PRIV_H_ */
